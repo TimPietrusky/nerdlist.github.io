81a8b40af5bbe1e076253498d872ad34254a4b8a
kevingimbel <hallo@kevingimbel.de>
2013-10-26 22:32:55 +0200

    small improvments to Sign-Up form

--- config.rb
+++ config.rb
@@ -2,11 +2,11 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
-sass_dir = "css"
-images_dir = "images"
-javascripts_dir = "js"
-fonts_dir = "fonts"
+css_dir = "assets/css"
+sass_dir = "assets/css"
+images_dir = "assets/img"
+javascripts_dir = "assets/js"
+fonts_dir = "assets/fonts"
 
 output_style = :expanded
 
